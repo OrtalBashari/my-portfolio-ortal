6f775ddc1fc9795cf8c78c6368c260808f02a596
OrtalBashari <ortalb352@gmail.com>
2025-10-04 18:10:00 +0300

    my portfolio

---
+++ script.js
@@ -0,0 +1,17 @@
+document.querySelector('form').addEventListener('submit', function(e){
+    e.preventDefault();
+    alert('Thank you, your message has been sent!');
+});
+
+
+const sections = document.querySelectorAll('.section');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        }
+    });
+}, { threshold: 0.2 });
+
+sections.forEach(section => observer.observe(section));
